1d52485e41e0ff30bd27e4ce24ac72ad40b8d89f
John Ginger <github@johnginger.co.uk>
2016-08-14 13:44:37 +0100

    Add accessing database

--- db.js
+++ db.js
@@ -14,3 +14,22 @@ const db = knex({
 
 module.exports = db;
 module.exports.clean = () => db.schema.raw("TRUNCATE data RESTART IDENTITY CASCADE;");
+
+module.exports.get = function(searchParameters){
+    
+    let query = db('data').where("sensorId", searchParameters.sensorId)
+
+    // In the readme it says that you should use an int - but the code implied strongly 
+    // and ISO string so I assumed you meant an iso string.
+
+    if (searchParameters.hasSince){
+        query = query.andWhere("time", ">", searchParameters.since.toISOString())
+    }
+
+    if (searchParameters.hasUntil){
+        query = query.andWhere("time", "<", searchParameters.until.toISOString())
+    }
+    
+    return query.select()
+
+}
